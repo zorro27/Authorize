ad59b2fbe24a723781905767f2212c62f7b64159
zorro27 <73430357+zorro27@users.noreply.github.com>
2023-06-28 22:06:58 +0300

    Initial Commit

---
+++ Authorize/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Authorize
+//
+//  Created by Роман Зобнин on 28.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
