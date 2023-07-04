e38337dd759b8703f742902b7863ecce9f7b6046
zorro27 <73430357+zorro27@users.noreply.github.com>
2023-07-04 21:57:58 +0300

    Создал стартовый экран. Удалил сториборд. Делаем ручками

---
+++ Authorize/StartViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StartViewController.swift
+//  Authorize
+//
+//  Created by Роман Зобнин on 04.07.2023.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+    
+
+   
+
+}
